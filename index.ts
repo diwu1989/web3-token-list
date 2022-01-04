bd8c47082e2a299b9b3ebb86736ed9c7c6493ec3
Brian Li <brnli7@gmail.com>
2022-01-03 20:14:19 -0800

    Add typescript

---
+++ index.ts
@@ -0,0 +1,26 @@
+import AvalancheTokens from "./build/43114-tokens.json";
+import CeloTokens from "./build/42220-tokens.json";
+import PolygonTokens from "./build/137-tokens.json";
+
+type Token = {
+    chainId: number;
+    address: string;
+    decimals: number;
+    name: string;
+    symbol: string;
+    logoURI: string;
+};
+
+const TOKEN_LIST: Record<number | string, Token[]> = {
+    // avalanche
+    43114: AvalancheTokens,
+    avalanche: AvalancheTokens,
+    // celo
+    42220: CeloTokens,
+    celo: CeloTokens,
+    // polygon
+    137: PolygonTokens,
+    polygon: PolygonTokens,
+};
+
+export default TOKEN_LIST;
