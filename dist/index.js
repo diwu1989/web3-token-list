51a64b5b7f4eeb0e1423b7c1cbe8d752cb7df01e
Brian Li <brnli7@gmail.com>
2022-01-03 20:18:28 -0800

    Add Alfajores tokens to index.ts

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const _43114_tokens_json_1 = __importDefault(require("./build/43114-tokens.json"));
 const _42220_tokens_json_1 = __importDefault(require("./build/42220-tokens.json"));
 const _137_tokens_json_1 = __importDefault(require("./build/137-tokens.json"));
+const _44787_tokens_json_1 = __importDefault(require("./build/44787-tokens.json"));
 const TOKEN_LIST = {
     // avalanche
     43114: _43114_tokens_json_1.default,
@@ -13,6 +14,9 @@ const TOKEN_LIST = {
     // celo
     42220: _42220_tokens_json_1.default,
     celo: _42220_tokens_json_1.default,
+    // alfajores
+    44787: _44787_tokens_json_1.default,
+    alfajores: _44787_tokens_json_1.default,
     // polygon
     137: _137_tokens_json_1.default,
     polygon: _137_tokens_json_1.default,
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import AvalancheTokens from "./build/43114-tokens.json";
 import CeloTokens from "./build/42220-tokens.json";
 import PolygonTokens from "./build/137-tokens.json";
+import AlfajoresTokens from "./build/44787-tokens.json";
 
 type Token = {
     chainId: number;
@@ -18,6 +19,9 @@ const TOKEN_LIST: Record<number | string, Token[]> = {
     // celo
     42220: CeloTokens,
     celo: CeloTokens,
+    // alfajores
+    44787: AlfajoresTokens,
+    alfajores: AlfajoresTokens,
     // polygon
     137: PolygonTokens,
     polygon: PolygonTokens,
